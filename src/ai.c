4bdd1fe530f12a0503a90ab1f9abfa8c5f598f55
TWQ1765 <www.taaadaaa@qq.com>
2018-06-21 10:46:15 +0800

    add .c and .h file

---
+++ src/ai.c
@@ -0,0 +1,16 @@
+#include "ai.h"
+
+
+int stringCompare(char *str1, char *str2){
+	
+	return 0;
+}
+
+
+char *speakToAiMachine(char *msg){
+	
+	
+	
+	return NULL;
+	
+}
---
+++ test/test_ai.c
@@ -0,0 +1,17 @@
+#include "unity.h"
+#include "ai.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_ai_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement ai");
+}
+
+
